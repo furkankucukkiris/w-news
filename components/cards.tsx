cbe9064c573e833b8da09f38d65ceef01118b4ba
furkankucukkiris <furkan.kucukkiris@codonxlabs.com>
2022-11-21 20:10:45 +0300

    css refactor

--- components/cards.tsx
+++ components/cards.tsx
@@ -10,8 +10,8 @@ export default function Cards() {
                     <Image src="/images/image-retro-pcs.jpg" alt="img" fill/>
                 </div>
                 <div className={styles.cardText}>
-                    <h3 className={styles.cardTitle}>01</h3>
-                    <h4 className={styles.cardSubTitle}>Reviving Retro PCs</h4>
+                    <div className={styles.cardTitle}>01</div>
+                    <div className={styles.cardSubTitle}>Reviving Retro PCs</div>
                     <p className={styles.cardP}>What happens when old PCs are given modern upgrades?</p>
                 </div>
             </div>
@@ -20,8 +20,8 @@ export default function Cards() {
                     <Image src="/images/image-top-laptops.jpg" alt="img" fill/>
                 </div>
                 <div className={styles.cardText}>
-                    <h3 className={styles.cardTitle}>02</h3>
-                    <h4 className={styles.cardSubTitle}>Top 10 Laptops of 2022</h4>
+                    <div className={styles.cardTitle}>02</div>
+                    <div className={styles.cardSubTitle}>Top 10 Laptops of 2022</div>
                     <p className={styles.cardP}>Our best picks for various needs and budgets.</p>
                 </div>
             </div>
@@ -30,8 +30,8 @@ export default function Cards() {
                     <Image src="/images/image-gaming-growth.jpg" alt="img" fill/>
                 </div>
                 <div className={styles.cardText}>
-                    <h3 className={styles.cardTitle}>03</h3>
-                    <h4 className={styles.cardSubTitle}>The Growth of Gaming</h4>
+                    <div className={styles.cardTitle}>03</div>
+                    <div className={styles.cardSubTitle}>The Growth of Gaming</div>
                     <p className={styles.cardP}>How the pandemic has sparked fresh opportunities.</p>
                 </div>
             </div>
